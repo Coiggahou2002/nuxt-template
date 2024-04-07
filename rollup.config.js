90a956fc522d05ed9a80cdd331622f8f528c6821
ccccccjh <72149771+Coiggahou2002@users.noreply.github.com>
2024-04-07 16:19:58 +0800

    chore: add node-resolve plugin for rollup to resolve external modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 // rollup.config.js
 import typescript from '@rollup/plugin-typescript';
 import alias from '@rollup/plugin-alias';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 export default {
 	input: 'index.ts',
 	output: {
@@ -8,14 +9,16 @@ export default {
 		format: 'cjs'
 	},
   plugins: [
-		typescript({
-			tsconfig: './tsconfig.json',
-			exclude: ['node_modules'],
-		}),
+		nodeResolve(),
 		alias({
       entries: [
         { find: '@', replacement: './src' },
       ]
-    })
+    }),
+		typescript({
+			tsconfig: './tsconfig.json',
+			exclude: ['node_modules'],
+		}),
+		
 	]
 };
