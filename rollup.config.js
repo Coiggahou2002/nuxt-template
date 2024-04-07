19ebaced91297edf4f566d4352e051cbaa5be7dd
ccccccjh <72149771+Coiggahou2002@users.noreply.github.com>
2024-04-07 11:16:45 +0800

    feat: add alias plugin to rollup to use @-like alias

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,21 @@
 // rollup.config.js
 import typescript from '@rollup/plugin-typescript';
+import alias from '@rollup/plugin-alias';
 export default {
 	input: 'index.ts',
 	output: {
 		file: 'bundle.js',
 		format: 'cjs'
 	},
-  plugins: [typescript()]
+  plugins: [
+		typescript({
+			tsconfig: './tsconfig.json',
+			exclude: ['node_modules'],
+		}),
+		alias({
+      entries: [
+        { find: '@', replacement: './src' },
+      ]
+    })
+	]
 };
